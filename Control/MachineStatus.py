5f8af42f44e7a6008bd766d4104d1a52cdf58b33
Stefan Siegl <stesie@brokenpipe.de>
2014-04-17 22:44:45 +0200

    show status info in frontend

--- Control/MachineStatus.py
+++ Control/MachineStatus.py
@@ -1,11 +1,18 @@
+from PySide import QtGui, QtCore
 
-class ControlMachineStatus:
+class ControlMachineStatus(QtCore.QObject):
 	status = None
-	Px = None
-	Py = None
-	Pz = None
+	pX = None
+	pY = None
+	pZ = None
+	wpX = 0
+	wpY = 0
+	wpZ = 0
+
+	statusUpdated = QtCore.Signal()
 
 	def __init__(self, chatBackend):
+		QtCore.QObject.__init__(self)
 		self._chatBackend = chatBackend
 
 	def updateStatus(self):
@@ -14,19 +21,20 @@ class ControlMachineStatus:
 
 		if res[0:2] != "@X":
 			raise ValueError("Unexpected reply to @X command: " + res)
-
 		self.status = res[2:]
 
-		if self.status == "14" or self.Px == None:
-			self.updateMachinePos("Px")
-			self.updateMachinePos("Py")
-			self.updateMachinePos("Pz")
+		if self.status == "14" or self.pX == None:
+			self.pX = self.fetchMachinePos("Px")
+			self.pY = self.fetchMachinePos("Py")
+			self.pZ = self.fetchMachinePos("Pz")
 
-	def updateMachinePos(self, direction):
+		self.statusUpdated.emit()
+
+	def fetchMachinePos(self, direction):
 		self._chatBackend.send("@" + direction)
 		res = self._chatBackend.getline()
 
 		if res[0:3] != "@" + direction.upper():
 			raise ValueError("Unexpected reply to @" + direction + " command: " + res)
+		return res[3:]
 
-		setattr(self, direction, res[3:])
--- Control/MainWindow.py
+++ Control/MainWindow.py
@@ -1,14 +1,33 @@
 from ui.MainWindow import Ui_MainWindow
-from PySide import QtGui
+from PySide import QtGui, QtCore
 
 from Control.MachineStatus import ControlMachineStatus
 
 class ControlMainWindow(QtGui.QMainWindow):
+	statusMessages = {
+		'04': 'ready',
+		'14': 'moving',
+		'15': 'ref movement',
+	}
+
 	def __init__(self, chatBackend):
 		super(ControlMainWindow, self).__init__(None)
 
 		self._status = ControlMachineStatus(chatBackend);
-		self._status.updateStatus()
+		self._status.statusUpdated.connect(self.statusUpdated)
 
 		self._ui = Ui_MainWindow()
 		self._ui.setupUi(self)
+
+		self._status.updateStatus()
+
+	@QtCore.Slot()
+	def statusUpdated(self):
+		self._ui.statusX.setText("%s (%s)" % (
+			self._status.status,
+			self.statusMessages[self._status.status]
+		))
+
+		self._ui.statusPx.setText(self._status.pX)
+		self._ui.statusPy.setText(self._status.pY)
+		self._ui.statusPz.setText(self._status.pZ)
