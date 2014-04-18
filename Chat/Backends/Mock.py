0b6584bb3241b6052824d2ca8b81ea86ddfeea3a
Stefan Siegl <stesie@brokenpipe.de>
2014-04-18 12:26:15 +0200

    make ChatBackendMock use MockMachine

--- Chat/Backends/Mock.py
+++ Chat/Backends/Mock.py
@@ -1,22 +1,19 @@
 from Chat.Backends.Base import ChatBackendBase
 from collections import deque
+from Control.MockMachine import MockMachine
 
 class ChatBackendMock(ChatBackendBase):
 	"""Mock chat backend implementations."""
 	_nextLines = deque()
+        _machine = MockMachine()
 
 	def send(self, sendStr, expectStr = None):
 		self._chatLog.append("out", sendStr)
 		# @todo expectStr
 
-		if sendStr == "@X":
-			self._nextLines.append("@X04")
-		elif sendStr == "@Px":
-			self._nextLines.append("@PX0123.456")
-		elif sendStr == "@Py":
-			self._nextLines.append("@PY0123.456")
-		elif sendStr == "@Pz":
-			self._nextLines.append("@PZ0123.456")
+                reply = self._machine.process(sendStr)
+                if reply != "":
+                        self._nextLines.append(reply)
 
 	def getline(self):
 		msg = self._nextLines.popleft()
