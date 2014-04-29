d6612e91dae9ea8a870a1ee8ef8fdfbc46de5780
Stefan Siegl <stesie@brokenpipe.de>
2014-04-29 22:21:54 +0200

    translation update

--- i18n/de_DE.ts
+++ i18n/de_DE.ts
@@ -4,249 +4,259 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="ui/MainWindow.py" line="338"/>
+        <location filename="ui/MainWindow.py" line="357"/>
         <source>PyPC-NC</source>
         <translation>PyPC-NC</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="339"/>
+        <location filename="ui/MainWindow.py" line="358"/>
         <source>Status Info</source>
         <translation>Status Info</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="340"/>
+        <location filename="ui/MainWindow.py" line="359"/>
         <source>@X</source>
         <translation>@X</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="341"/>
+        <location filename="ui/MainWindow.py" line="360"/>
         <source>04 (ready)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="342"/>
+        <location filename="ui/MainWindow.py" line="361"/>
         <source>@PX</source>
         <translation>@PX</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="358"/>
+        <location filename="ui/MainWindow.py" line="379"/>
         <source>123</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="344"/>
+        <location filename="ui/MainWindow.py" line="363"/>
         <source>@PY</source>
         <translation>@PY</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="346"/>
+        <location filename="ui/MainWindow.py" line="365"/>
         <source>@PZ</source>
         <translation>@PZ</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="348"/>
+        <location filename="ui/MainWindow.py" line="367"/>
         <source>@PU</source>
         <translation>@PU</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="352"/>
+        <location filename="ui/MainWindow.py" line="373"/>
         <source>Workpiece Relative Location</source>
         <translation>Relative Position des Werkstücks</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="353"/>
+        <location filename="ui/MainWindow.py" line="374"/>
         <source>X</source>
         <translation>X</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="355"/>
+        <location filename="ui/MainWindow.py" line="376"/>
         <source>Y</source>
         <translation>Y</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="357"/>
+        <location filename="ui/MainWindow.py" line="378"/>
         <source>Z</source>
         <translation>Z</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="359"/>
+        <location filename="ui/MainWindow.py" line="380"/>
         <source>Goto ...</source>
         <translation>Anfahren ...</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="360"/>
+        <location filename="ui/MainWindow.py" line="381"/>
         <source>Store ...</source>
         <translation>Speichern ...</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="361"/>
+        <location filename="ui/MainWindow.py" line="382"/>
         <source>Manual Movement</source>
         <translation>Manuelle Fahrt</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="362"/>
+        <location filename="ui/MainWindow.py" line="383"/>
         <source>X-</source>
         <translation>X-</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="363"/>
+        <location filename="ui/MainWindow.py" line="384"/>
         <source>X+</source>
         <translation>X+</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="364"/>
+        <location filename="ui/MainWindow.py" line="385"/>
         <source>U+</source>
         <translation>U+</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="365"/>
+        <location filename="ui/MainWindow.py" line="386"/>
         <source>U-</source>
         <translation>U-</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="366"/>
+        <location filename="ui/MainWindow.py" line="387"/>
         <source>Y+</source>
         <translation>Y+</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="367"/>
+        <location filename="ui/MainWindow.py" line="388"/>
         <source>Y-</source>
         <translation>Y-</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="368"/>
+        <location filename="ui/MainWindow.py" line="389"/>
         <source>Z+</source>
         <translation>Z+</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="369"/>
+        <location filename="ui/MainWindow.py" line="390"/>
         <source>Z-</source>
         <translation>Z-</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="370"/>
+        <location filename="ui/MainWindow.py" line="391"/>
         <source>1 Step</source>
         <translation>1 Schritt</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="371"/>
+        <location filename="ui/MainWindow.py" line="392"/>
         <source>0.01 mm</source>
         <translation>0.01 mm</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="372"/>
+        <location filename="ui/MainWindow.py" line="393"/>
         <source>0.1 mm</source>
         <translation>0.1 mm</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="373"/>
+        <location filename="ui/MainWindow.py" line="394"/>
         <source>1 mm</source>
         <translation>1 mm</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="374"/>
+        <location filename="ui/MainWindow.py" line="395"/>
         <source>10 mm</source>
         <translation>10 mm</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="375"/>
+        <location filename="ui/MainWindow.py" line="396"/>
         <source>100 mm</source>
         <translation>100 mm</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="376"/>
+        <location filename="ui/MainWindow.py" line="397"/>
         <source>fast</source>
         <translation>schnell</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="377"/>
+        <location filename="ui/MainWindow.py" line="398"/>
         <source>slow</source>
         <translation>langsam</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="378"/>
+        <location filename="ui/MainWindow.py" line="399"/>
         <source>STOP</source>
         <translation>STOP</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="379"/>
+        <location filename="ui/MainWindow.py" line="400"/>
         <source>Ref. Movement</source>
         <translation>Referenzfahrt</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="380"/>
+        <location filename="ui/MainWindow.py" line="401"/>
         <source>Import G-Code</source>
         <translation>G-Code importieren</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="381"/>
+        <location filename="ui/MainWindow.py" line="402"/>
         <source>Goto</source>
         <translation>Anfahren</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="382"/>
+        <location filename="ui/MainWindow.py" line="403"/>
         <source>Store</source>
         <translation>Speichern</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="383"/>
+        <location filename="ui/MainWindow.py" line="404"/>
         <source>Goto XY</source>
         <translation>Anfahren XY</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="384"/>
+        <location filename="ui/MainWindow.py" line="405"/>
         <source>Goto XYZ</source>
         <translation>Anfahren XYZ</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="385"/>
+        <location filename="ui/MainWindow.py" line="406"/>
         <source>Goto X</source>
         <translation>Anfahren X</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="386"/>
+        <location filename="ui/MainWindow.py" line="407"/>
         <source>Goto Y</source>
         <translation>Anfahren Y</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="387"/>
+        <location filename="ui/MainWindow.py" line="408"/>
         <source>Goto Z</source>
         <translation>Anfahren Z</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="388"/>
+        <location filename="ui/MainWindow.py" line="409"/>
         <source>Store XY</source>
         <translation>Speichern XY</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="389"/>
+        <location filename="ui/MainWindow.py" line="410"/>
         <source>Store XYZ</source>
         <translation>Speichern XYZ</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="390"/>
+        <location filename="ui/MainWindow.py" line="411"/>
         <source>Store X</source>
         <translation>Speichern X</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="391"/>
+        <location filename="ui/MainWindow.py" line="412"/>
         <source>Store Y</source>
         <translation>Speichern Y</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="392"/>
+        <location filename="ui/MainWindow.py" line="413"/>
         <source>Store Z</source>
         <translation>Speichern Z</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="350"/>
+        <location filename="ui/MainWindow.py" line="369"/>
         <source>Options</source>
         <translation>Optionen</translation>
     </message>
     <message>
-        <location filename="ui/MainWindow.py" line="351"/>
+        <location filename="ui/MainWindow.py" line="370"/>
         <source>Invert Z-axis</source>
         <translation>Z-Achse invertieren</translation>
+    </message>
+    <message>
+        <location filename="ui/MainWindow.py" line="371"/>
+        <source>Feed rate override</source>
+        <translation>Geschwindigkeitsoverride</translation>
+    </message>
+    <message>
+        <location filename="ui/MainWindow.py" line="372"/>
+        <source>Work progress</source>
+        <translation>Arbeitsfortschritt</translation>
     </message>
 </context>
 </TS>
