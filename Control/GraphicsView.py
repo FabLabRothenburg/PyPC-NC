ed692ef9ef3128b49a24c0697b4bc716b8592a83
Stefan Siegl <stesie@brokenpipe.de>
2014-07-09 21:06:49 +0200

    add graphics view cursor

--- Control/GraphicsView.py
+++ Control/GraphicsView.py
@@ -1,3 +1,4 @@
+import math
 from PySide import QtGui, QtCore
 
 class ControlGraphicsView(QtGui.QDialog):
@@ -8,7 +9,7 @@ class ControlGraphicsView(QtGui.QDialog):
 		self._ui.setupUi(self)
 
 	def render(self, parser):
-		self._scene = QtGui.QGraphicsScene()
+		self._scene = MyGraphicsScene()
 
 		renderer = SceneRenderer(self._scene)
 		inter = GCode.GCodeInterpreter(renderer)
@@ -26,6 +27,52 @@ class ControlGraphicsView(QtGui.QDialog):
 		bbox.setRight(bbox.right() + bbox.width() * 0.1)
 		self._scene.setSceneRect(bbox)
 		self._ui.graphicsView.fitInView(bbox, QtCore.Qt.KeepAspectRatio)
+		self._ui.graphicsView.autoconfCursorRadius()  # @fixme wait for initial resize
+
+class MyGraphicsScene(QtGui.QGraphicsScene):
+	def __init__(self):
+		super(MyGraphicsScene, self).__init__()
+
+		pen = QtGui.QPen(QtCore.Qt.GlobalColor.red)
+		brush = QtGui.QBrush(QtCore.Qt.GlobalColor.red)
+		self._cursor = self.addEllipse(0, 0, 0, 0, pen, brush)
+
+	def mousePressEvent(self, event):
+		super(MyGraphicsScene, self).mousePressEvent(event)
+		self._mousePressPos = event.screenPos()
+
+	def mouseReleaseEvent(self, event):
+		super(MyGraphicsScene, self).mouseReleaseEvent(event)
+		if event.button() != QtCore.Qt.LeftButton: return
+		if self._mousePressPos.x() != event.screenPos().x(): return
+		if self._mousePressPos.y() != event.screenPos().y(): return
+
+		clickPos = event.buttonDownScenePos(QtCore.Qt.LeftButton)
+		items = self.items(clickPos.x() - 1000, clickPos.y() - 1000, 2000, 2000, QtCore.Qt.IntersectsItemShape)
+		if not len(items): return
+
+		maxDist = None
+		selPos = None
+		for item in items:
+			if not isinstance(item, QtGui.QGraphicsLineItem): continue
+			for point in [ item.line().p1(), item.line().p2() ]:
+				dist = math.sqrt((point.x() - clickPos.x()) ** 2 + (point.y() - clickPos.y()) ** 2)
+
+				if maxDist == None or dist < maxDist:
+					maxDist = dist
+					selPos = point
+
+		self.setCursorPosition(selPos)
+
+	def setCursorPosition(self, pos):
+		d = self._cursor.rect().width()
+		self._cursor.setRect(pos.x() - d / 2, pos.y() - d / 2, d, d)
+
+	def setCursorRadius(self, newr):
+		oldr = self._cursor.rect().width() / 2
+		x = self._cursor.rect().x() + oldr
+		y = self._cursor.rect().y() + oldr
+		self._cursor.setRect(x - newr, y - newr, newr * 2, newr * 2)
 
 class MyGraphicsView(QtGui.QGraphicsView):
 	def __init__(self, parent):
@@ -44,6 +91,13 @@ class MyGraphicsView(QtGui.QGraphicsView):
 		if event.delta() < 0:
 			factor = 1.0 / factor
 		self.scale(factor, factor)
+		self.autoconfCursorRadius()
+
+	def autoconfCursorRadius(self):
+		bbox = self.mapToScene(self.viewport().geometry()).boundingRect()
+		r = 4 * bbox.width() / self.rect().width()
+		self.scene().setCursorRadius(r)
+
 
 
 
